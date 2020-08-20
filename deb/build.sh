92a009884880909c3ee15d2e4190e47048399d9f
Todd G <todd@dapix.io>
2020-08-19 22:45:41 -0600

    builds 2.0.0

--- deb/build.sh
+++ deb/build.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+chmod 0755 ./fio/usr/local/bin/*
+rm -f ./fioprotocol-*.deb
+rm -f ./fioprotocol-*.deb.asc
+
 if [ ! -f fio/usr/local/bin/fio-nodeos ] ; then
 	echo " *** ERROR *** binaries are missing, copy compiled files to:"
         echo "  ./fio/usr/local/bin/fio-nodeos"
+        echo "  ./fio/usr/local/bin/clio"
         echo "  ./fio/usr/local/bin/fio-keosd "
-        echo "  ./fio/usr/local/bin/clio "
 	exit 1
 fi
 
-VER=$(date -u +%Y%m%d%H%M)
+#VER="1.1.0-$(date -u +%Y%m%d%H%M)"
+NOW=$(date -u +%Y%m%d%H%M)
+VER="2.0.0"
 
 rm -f fio.deb
-sed -i ".bak" "s/xxxxxxxxxxxx/$VER/" fio/DEBIAN/control
+sed -i ".bak" "s/xxxxxxxxxxxx/$NOW/" fio/DEBIAN/control
 dpkg-deb --build --root-owner-group fio
-mv fio.deb fioprotocol-1.1.x-$VER-ubuntu-18.04-amd64.deb
-sed -i ".bak" "s/$VER/xxxxxxxxxxxx/" fio/DEBIAN/control
-
+mv fio.deb fioprotocol-$VER-ubuntu-18.04-amd64.deb
+sed -i ".bak" "s/$NOW/xxxxxxxxxxxx/" fio/DEBIAN/control
